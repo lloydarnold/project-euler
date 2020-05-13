7dd5af5dd3a40c14cdc40d9a9f6329a265201e53
Lloyd Arnold <lloarn@gmail.com>
2020-05-13 11:05:23 +0100

    Add solution to euler 5

---
+++ problem_5.py
@@ -0,0 +1,10 @@
+## lcm is product of prime factors.
+
+def eulerSolution():
+    print( 2*2*2*2 * 3*3 * 5 * 7 * 11 * 13 * 17 * 19 )
+
+def main():
+    eulerSolution()
+
+if __name__ == "__main__":
+    main()
