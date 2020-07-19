7c26bddfd8b225bca4f7815ec0ef8d3d5c8caa97
Lloyd Arnold <lloarn@gmail.com>
2020-07-19 11:18:38 +0100

    Solve 19

--- problem_019.py
+++ problem_019.py
@@ -1,20 +1,26 @@
 ## We'll be we'll be counting Sundays
 
 def moveDayPointer(pointer, numOfDays):
-    return
+    return (pointer + numOfDays) % 7
 
 def main():
     oneHotDays = [0,0,0,0,0,0,1]  ## onehot encode days - 1 is Sunday
     months = [31,28,31,30,31,30,31,31,30,31,30,31]
     numOfSuns = 0
-    
+
     ## root day -- Tuesday, January 1st, 1901
     dayPointer = 1
     monthPointer = 12
 
-    for year in range(1901, 2002):
+    for year in range(1901, 2001):
         for month in range(0,12):
+            if year % 4 == 0 and month == 1:
+                dayPointer = moveDayPointer(dayPointer, 29)
+            else :
+                dayPointer = moveDayPointer(dayPointer, months[month])
+
             numOfSuns += oneHotDays[dayPointer]
+    print(numOfSuns)
 
 
 if __name__ == '__main__':
